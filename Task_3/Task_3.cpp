1e17a24a29ac5651401cea5d10519c96d91d3c0e
Andrei_Gridin <andreigridin918@gmail.com>
2024-07-05 21:05:48 +0500

    Откорректировал по ресурсам

--- Task_3/Task_3.cpp
+++ Task_3/Task_3.cpp
@@ -4,6 +4,33 @@
 
 using namespace std;
 
+void show_vec(vector<int>& v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (i == 0)cout << "{ " << v[i] << ',';
+        else
+        {
+            if (i == v.size() - 1)cout << ' ' << v[i] << " }";
+            else cout << ' ' << v[i] << ',';
+        }
+    }
+}
+
+void bubble_sort(vector<int>& v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v.size() - 1 - i; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                swap(v[j], v[j + 1]);
+            }
+        }
+    }
+}
+
 int main()
 {
     system("color 80");
@@ -19,29 +46,31 @@ int main()
 
         if (n != -1)
         {
-            vec.push_back(n);
-
-            for (int i = 0; i < vec.size(); i++)
+            if (vec.size() < 5)
             {
-                for (int j = 0; j < vec.size() - 1 - i; j++)
+                vec.push_back(n);
+                bubble_sort(vec);
+            }
+            else
+            {
+                if (vec[4] > n)
                 {
-                    if (vec[j] > vec[j + 1])
-                    {
-                        swap(vec[j], vec[j + 1]);
-                    }
+                    vec.pop_back();
+                    vec.push_back(n);
+                    bubble_sort(vec);
                 }
             }
+            
         }
         else
         {
-            if (vec.size() >= 5)
+            if (vec.size() == 5)
             {
-                for (int i = 0; i < vec.size(); i++)
-                {
-                    cout << ' ' << vec[i];
-                }
                 cout << endl;
-                cout << ' ' << vec[4] << endl;
+                cout << " Вывод: " << vec[4];
+                cout << " (в отсортированном виде введённые числа выглядят так: ";
+                show_vec(vec);
+                cout << ")." << endl;
             }
         }
     }
